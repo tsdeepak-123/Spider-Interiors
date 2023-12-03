751f3a921852a4c3d0df3805829d68e1f897a43e
Deepak ts <tsdeepak589@gmail.com>
2023-12-04 03:23:33 +0530

    files and folders created in frontend and backend server started

---
+++ frontend/src/Routes/UserRoutes.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+
+function UserRoutes() {
+  return (
+    <div>
+      <Routes>
+        <Route path='' element={""}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default UserRoutes
